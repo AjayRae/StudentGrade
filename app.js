e16ce9aa7c6c431c03b4ed5d96a08966c8609b8b
Ajay <ajayrayan1411999@gmail.com>
2024-07-05 10:02:03 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,45 @@
+// tasks ---
+// should calculate total
+// calculate percentage
+// calculate grade
+// calculate pass/fail
+// show output
+
+
+const calcy = () =>{
+  let Tam = document.getElementById('tam').value;
+  let Eng = document.getElementById('eng').value;
+  let Mat = document.getElementById('math').value;
+  let Sci = document.getElementById('sci').value;
+  let Soc = document.getElementById('soc').value;
+  let grades = "";
+
+
+  let totalGrades = (parseFloat(Tam) + parseFloat(Eng) + parseFloat(Mat) + parseFloat(Sci)+ parseFloat(Soc)).toFixed(2);
+
+  let perc =(((totalGrades/500) * 100).toFixed(2));
+  
+
+  debugger;
+
+  if(perc <= 100  && perc >= 80){
+    grades = 'A';
+  }else if(perc <= 79  && perc >= 60){
+     grades = 'B';
+  }else if(perc <= 59  && perc >= 40){
+     grades = 'C';
+  }else{
+     grades = 'F';
+  }
+
+  if(perc >= 39.5){
+    document.getElementById('showData').innerHTML = ` Out of 500  <br>  <br>  Total is  ${totalGrades}  <br>  <br>  percentage is ${perc}%. <br>  <br> Your grade is ${grades}.  `
+    document.getElementById('result').innerHTML = ` PASS `
+  }
+  else{
+    document.getElementById('showData').innerHTML = ` Out of 500 your total is  ${totalGrades} and percentage is ${perc}%. <br> Your grade is ${grades}. `
+    document.getElementById('result').innerHTML = ` FAIL `
+  }
+
+  
+}
